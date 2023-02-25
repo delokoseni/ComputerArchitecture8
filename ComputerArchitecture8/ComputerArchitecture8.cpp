edb10e0b900fab07d19296ec5d76f0196a84646a
delokoseni <artur.b3lsh@gmail.com>
2023-02-25 13:45:41 +0700

    Добавьте файлы проекта.

---
+++ ComputerArchitecture8/ComputerArchitecture8.cpp
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include "stdio.h"
+#include <Windows.h>
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int n = 8, a[8], i;
+	for (i = 0; i < n; i++) {
+		printf("Введите a[%d%s", i, "]: ");
+		scanf("%d", &a[i]);
+	}
+	_asm {
+
+	}
+	return 0;
+}
